a3e5ee09c6b24a74042251d0cb6500f176c643d2
David <gidlos55@gmail.com>
2024-12-24 21:24:45 +0300

    задание 2 из 5 модуля

--- js/functions.js
+++ js/functions.js
@@ -22,3 +22,32 @@ function checkIfNumber(string) {
   return resultArray.length === 0 ? NaN : resultArray.join('');
 }
 checkIfNumber('анапа 2007');
+
+function checkMeeting(startTime, endTime, startTimeOfMeeting, meetingTime) {
+  const meetingTimeString = meetingTime.toString();
+  const timeBoard = [startTime, endTime, startTimeOfMeeting];
+  const timeBoardInMinutes = [];
+
+  for (let i = 0; i <= timeBoard.length - 1; i++) {
+    const time = timeBoard[i];
+    const timeSplited = time.split(':');
+    const hours = Number(timeSplited[0]);
+    const minutes = Number(timeSplited[1]);
+    const minutesTotal = hours * 60 + minutes;
+    timeBoardInMinutes.push(minutesTotal);
+  }
+  const [workDayStart, workDayEnd, meetingStartTime, durationOfMeeting] =
+    timeBoardInMinutes;
+
+  timeBoardInMinutes.push(meetingTimeString);
+
+  if (meetingStartTime < workDayStart) {
+    return false;
+  }
+  if (workDayEnd - meetingStartTime - durationOfMeeting >= 0) {
+    return true;
+  }
+  return false;
+}
+
+checkMeeting('8:0', '10:0', '8:0', 120);
