d055d340bbbb6418ac8c044570078a69ef38d37d
David <gidlos55@gmail.com>
2025-02-12 22:22:57 +0300

    Проходим пул

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { generateUserInterface } from './miniature.js';
 import { showErrorMessage } from './error-message.js';
 import './upload-form.js';
 import { getData } from './api.js';
-
+//ghdbtn
 getData()
   .then((data) => generateUserInterface(data))
   .catch((err) => {
