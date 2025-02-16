c025aa39bd6b4ac4d6e13b1f493956144eb9528b
David <gidlos55@gmail.com>
2025-02-16 17:44:56 +0300

    Добавил загрузку пользователького фото + фильтрация постов других пользователей (все тесты проходят)

---
+++ js/user-photo-upload.js
@@ -0,0 +1,23 @@
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
+
+const fileChooser = document.querySelector('#upload-file');
+const previewPicture = document.querySelector('.img-upload__preview img');
+const previewFilters = document.querySelectorAll('.effects__preview');
+
+fileChooser.addEventListener('change', () => {
+  const file = fileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((fileType) => fileName.endsWith(fileType));
+
+  if (matches) {
+    const imageUrl = URL.createObjectURL(file);
+    previewPicture.src = imageUrl;
+
+    previewFilters.forEach((filter) => {
+      filter.style.backgroundImage = `url("${imageUrl}")`;
+    });
+  }
+});
+
+export {};
